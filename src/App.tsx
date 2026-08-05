9d24033a8e40f9fe6540083a476399626ffb2e4e
Huber Maximilian <HubMax@HUBER.WAN>
2026-08-05 17:33:55 +0200

    feat: add favorites page

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import Trash from './pages/Trash'
+import Scan from './pages/Scan'
 import Archive from './pages/Archive'
 import Favorites from './pages/Favorites'
-import Scan from './pages/Scan'
+import Trash from './pages/Trash'
 
 export default function App() {
   return (
-    <Router>
-      <div style={{ minHeight: '100vh', backgroundColor: '#0f172a' }}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/archive" element={<Archive />} />
-          <Route path="/favorites" element={<Favorites />} />
-          <Route path="/scan" element={<Scan />} />
-          <Route path="/trash" element={<Trash />} />
-        </Routes>
-      </div>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/scan" element={<Scan />} />
+      <Route path="/archive" element={<Archive />} />
+      <Route path="/favorites" element={<Favorites />} />
+      <Route path="/trash" element={<Trash />} />
+    </Routes>
   )
 }
